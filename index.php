bbebb3955b48abe4ca842d10cf7d2cdd5bbd9ea7
Arkadiusz Klenczar <a.klenczar@gmail.com>
2014-11-16 12:58:38 +0100

    first

---
+++ index.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/blog.css" rel="stylesheet">
+
+</head>
+</html>
+<div class="blog-masthead">
+      <div class="container">
+        <nav class="blog-nav">
+          <a class="blog-nav-item active" href="#">Home</a>
+          <a class="blog-nav-item" href="#">New features</a>
+          <a class="blog-nav-item" href="#">Press</a>
+          <a class="blog-nav-item" href="#">New hires</a>
+          <a class="blog-nav-item" href="#">About</a>
+        </nav>
+      </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+
+        <div class="col-sm-12 blog-main">
+
+          <div class="blog-post">
+            <h2 class="blog-post-title">Portfolio</h2>
+            <p class="blog-post-meta">15.11.2014</p>
+
+            <?php
+//zadania projektowe
+echo "vhost - testowy";
+echo "<br/>";
+echo "<a href='frontend.php'>&raquo; Frontend</a>";
+echo "<ul>
+<li>Platforma docelowa</li>
+<p>Wybór platformy docelowej - Android. Urządzenia typu tablet. Ekran > 7\"</>
+<li>Funkcjonalności portfolio</li>
+<p>Wyświetlanie informacji/zdjęć z RDBMS. Galeria: podział na kategorie, przewijanie zdjęć, powrót do kategorii wyżej.</p>
+<li>Design portfolio</li>
+<p>Projekt designu dla galerii.</p>
+<li>Funkcjonalności backendu</li>
+<p>Dodawanie/usuwanie/edycja - kategorii, zdjęć.</p>
+<li>Design backendu</li>
+<p>Panel administracyjny dla funkcjonalności</p>
+<li>Test</li>
+<p>Testy na urządzeniach z systemem Android.</p>
+</ul>";
+
+?>
+          </div>
+            </div>
+        </div>
+
+
+
+    </div>
